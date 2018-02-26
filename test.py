e028110e604f326947a74dde3a5ca81c6809f115
Pedro V <hdez.serrano@gmail.com>
2018-02-26 17:55:10 +0100

    data integration

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import requests
-import json
-from pandas.io.json import json_normalize
-
-request = requests.get('https://dataderden.cbs.nl/ODataApi/OData/47004NED/TypedDataSet')
-table = json_normalize(json.loads(request.text)['value'])
-table.to_csv('test.csv')
